f7ac0bc3054ab7b8e78808b964964b0256cbf6aa
Nick Teaford <nickteaford@Nicks-iMac.local>
2022-01-21 11:33:59 -0800

    complete step 3.1

--- main_app/models.py
+++ main_app/models.py
@@ -3,9 +3,8 @@ from django.forms import CharField
 
 # Create your models here.
 class Widget(models.Model):
-    name = models.CharField(max_length=30),
     description = models.CharField(max_length=100),
     quantity = models.IntegerField(),
 
     def __str__(self):
-        return self.name
+        return self.description
